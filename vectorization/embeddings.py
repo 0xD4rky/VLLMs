ee69e080f0897a37df77dd40d580031b1ac17d2f
0xD4rky <ishaan.kumar281@gmail.com>
2024-10-18 23:09:41 +0530

    visualizing embeddings

--- vectorization/embeddings.py
+++ vectorization/embeddings.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 # Load pre-trained ResNet model
 model = models.resnet50(pretrained=True)
-model = torch.nn.Sequential(*list(model.children())[:-1])
+model = torch.nn.Sequential(*list(model.children())[:-1]) # -> new model contains all layers except the last one
 model.eval()
 
 transform = transforms.Compose([
@@ -14,7 +14,7 @@ transform = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
-image = Image.open(r'sukuna.jpg')
+image = Image.open(r'/home/darky/Documents/dyna_vec/vectorization/test.jpg')
 input_tensor = transform(image).unsqueeze(0)
 
 with torch.no_grad():
