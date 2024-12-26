2cc42d6fd44310ab2ee4711ba0883367585d2f64
0xD4rky <ishaan.kumar281@gmail.com>
2024-12-26 22:45:50 +0530

    mapping and batching embeddings

--- faiss.py
+++ faiss.py
@@ -22,6 +22,33 @@ def vis_data(dataset):
     print(f"train dataset : {dataset['train']}")
     print(f"example image of train dataset : {dataset['train'][0]['image']}")
     print(f"example caption of train dataset : {dataset['train'][0]['image_description']}")
+    features = ['image', 'contest_number', 'image_location', 'image_description', 'image_uncanny_description', 'entities', 'questions', 'caption_choices', 'from_description', 'label', 'n_tokens_label', 'instance_id']
+    for i,ff in enumerate(features):
+        print(f"{i+1}th feature is {ff} and it is : ")
+        print(dataset["train"][0][ff])
+
 
 # if i need to visualize data --> vis_data(dataset)
 
+data = dataset["train"]
+ds_with_embeddings = data.map(
+    lambda example: {
+        "embeddings": model.get_text_features(
+            **tokenizer([example["image_description"]], truncation=True, return_tensors="pt").to("cuda")
+        )[0]
+        .detach()
+        .cpu()
+        .numpy()
+    }
+)
+
+ds_with_embeddings = ds_with_embeddings.map(
+    lambda example: {
+        "image_embeddings": model.get_image_features(**processor([example["image"]], return_tensors="pt").to("cuda"))[
+            0
+        ]
+        .detach()
+        .cpu()
+        .numpy()
+    }
+)
