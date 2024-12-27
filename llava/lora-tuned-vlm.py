7a8c5d6c27a7e2e3134aecd31b282c0ba64e0c0c
0xD4rky <ishaan.kumar281@gmail.com>
2024-12-27 23:00:36 +0530

    quant model

--- llava/lora-tuned-vlm.py
+++ llava/lora-tuned-vlm.py
@@ -4,3 +4,18 @@ import torch
 from trl import DPOConfig, DPOTrainer
 from peft import LoraConfig
 import torch.quantization as tq
+
+def main():
+
+    print("Loading model...")
+    model = AutoModelForVision2Seq.from_pretrained("HuggingFaceM4/idefics2-8b", torch_dtype=torch.bfloat16)
+    processor = AutoProcessor.from_pretrained("HuggingFaceM4/idefics2-8b", do_image_splitting=False)
+
+    print("Loading dataset...")
+    dataset = load_dataset("openbmb/RLAIF-V-Dataset", split="train")
+
+    model = tq.quantize_dynamic(
+        model, 
+        {torch.nn.Linear},  
+        dtype=torch.qint8  
+    )
