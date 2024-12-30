0dab62984881a0feb49a00ff9132948edba1e710
0xD4rky <ishaan.kumar281@gmail.com>
2024-12-31 00:00:47 +0530

    lora layers

--- vectorization/lora-from-scratch.py
+++ vectorization/lora-from-scratch.py
@@ -18,6 +18,16 @@ class LoRALayer(torch.nn.Module):
         x = self.alpha * (x @ self.A @ self.B)
         return x
     
+class LinearWithLoRA(torch.nn.Module):
+    def __init__(self, linear, rank, alpha):
+        super().__init__()
+        self.linear = linear
+        self.lora = LoRALayer(
+            linear.in_features, linear.out_features, rank, alpha
+        )
+
+    def forward(self, x):
+        return self.linear(x) + self.lora(x)
 
 """
 LoRA is a method that can be applied to various types of neural networks, 
@@ -30,3 +40,39 @@ from transformers import AutoModelForSequenceClassification
 
 model = AutoModelForSequenceClassification.from_pretrained(
     "distilbert-base-uncased", num_labels=2)
+
+for param in model.parameters():
+    param.requires_grad = False
+
+from functools import partial
+
+# default hyperparameter choices
+lora_r = 8
+lora_alpha = 16
+lora_dropout = 0.05
+lora_query = True
+lora_key = False
+lora_value = True
+lora_projection = False
+lora_mlp = False
+lora_head = False
+
+layers = []
+
+assign_lora = partial(LinearWithLoRA, rank=lora_r, alpha=lora_alpha)
+
+for layer in model.distilbert.transformer.layer:
+    if lora_query:
+        layer.attention.q_lin = assign_lora(layer.attention.q_lin)
+    if lora_key:
+        layer.attention.k_lin = assign_lora(layer.attention.k_lin)
+    if lora_value:
+        layer.attention.v_lin = assign_lora(layer.attention.v_lin)
+    if lora_projection:
+        layer.attention.out_lin = assign_lora(layer.attention.out_lin)
+    if lora_mlp:
+        layer.ffn.lin1 = assign_lora(layer.ffn.lin1)
+        layer.ffn.lin2 = assign_lora(layer.ffn.lin2)
+if lora_head:
+    model.pre_classifier = assign_lora(model.pre_classifier)
+    model.classifier = assign_lora(model.classifier)
