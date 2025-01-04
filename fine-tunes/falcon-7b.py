d05eab229eafe9220eea34009eec4a5f7350191a
0xD4rky <ishaan.kumar281@gmail.com>
2025-01-04 20:12:53 +0530

    lora module

--- fine-tunes/falcon-7b.py
+++ fine-tunes/falcon-7b.py
@@ -17,3 +17,22 @@ model.config.use_cache = False
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 tokenizer.pad_token = tokenizer.eos_token
 
+from peft import LoraConfig
+
+lora_alpha = 16
+lora_dropout = 0.1
+lora_r = 64
+
+peft_config = LoraConfig(
+    lora_alpha=lora_alpha,
+    lora_dropout=lora_dropout,
+    r=lora_r,
+    bias="none",
+    task_type="CAUSAL_LM",
+    target_modules=[
+        "query_key_value",
+        "dense",
+        "dense_h_to_4h",
+        "dense_4h_to_h",
+    ]
+)
