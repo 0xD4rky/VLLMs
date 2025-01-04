fb5105b41481243e83bfbe4d4dae802c187fb6ef
0xD4rky <ishaan.kumar281@gmail.com>
2025-01-04 20:19:51 +0530

    building training args

--- fine-tunes/falcon-7b.py
+++ fine-tunes/falcon-7b.py
@@ -36,3 +36,37 @@ peft_config = LoraConfig(
         "dense_4h_to_h",
     ]
 )
+
+from transformers import TrainingArguments
+from trl import SFTTrainer, SFTConfig
+
+output_dir = "./results"
+per_device_train_batch_size = 4
+gradient_accumulation_steps = 4
+optim = "paged_adamw_32bit"
+save_steps = 10
+logging_steps = 10
+learning_rate = 2e-4
+max_grad_norm = 0.3
+max_steps = 500
+warmup_ratio = 0.03
+lr_scheduler_type = "constant"
+
+training_arguments = SFTConfig(
+    output_dir=output_dir,
+    per_device_train_batch_size=per_device_train_batch_size,
+    gradient_accumulation_steps=gradient_accumulation_steps,
+    optim=optim,
+    save_steps=save_steps,
+    logging_steps=logging_steps,
+    learning_rate=learning_rate,
+    fp16=True,
+    max_grad_norm=max_grad_norm,
+    max_steps=max_steps,
+    max_seq_length = 512,
+    warmup_ratio=warmup_ratio,
+    group_by_length=True,
+    lr_scheduler_type=lr_scheduler_type,
+    gradient_checkpointing=True,
+)
+
